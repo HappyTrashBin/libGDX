9c6cd87065f61776d6d010c195dfe461f916c45e
HappyTrashBin <happytrashbin911@gmail.com>
2024-04-06 19:20:46 +0300

    Working collision with main character

--- core/src/com/mygdx/game/Const.java
+++ core/src/com/mygdx/game/Const.java
@@ -1,9 +1,13 @@
 package com.mygdx.game;
 
 public class Const {
-    public int size = 64;
-    public int width = 1600;
-    public int height = 900;
-    public int FPS = 60;
+    public final int size = 64;
+    public final int width = 1600;
+    public final int height = 900;
+    public final int FPS = 60;
+    public final int speedConst = 2;
+    public final boolean genNewEnemies = false;
+    public final float timeSeconds = 0f;
+    public final float period = 1f;
 
 }
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,7 +2,6 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
@@ -10,11 +9,13 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class MyGdxGame extends ApplicationAdapter {
 	private Const constant = new Const();
+	private final float size = constant.size;
 	private final float halfSize = constant.size/2;
 	private final float width = constant.width;
 	private final float height = constant.height;
@@ -23,13 +24,15 @@ public class MyGdxGame extends ApplicationAdapter {
 	private final List<mainChar> enemies = new ArrayList<>();
 	private keyboardAdapter inputProcessor = new keyboardAdapter();
 	private final Vector2 goTo = new Vector2(1,1);
-	private final int speedConst = 2;
+	private final int speedConst = constant.speedConst;
+	private float timeSeconds = constant.timeSeconds;
+	private float period = constant.period;
 	@Override
 	public void create () {
 		Gdx.input.setInputProcessor(inputProcessor);
 		batch = new SpriteBatch();
 		me = new mainChar(width/2 - halfSize, height/2 - halfSize);
-		List<mainChar> newEnemies = IntStream.range(0,15)
+		List<mainChar> newEnemies = IntStream.range(0,2)
 				.mapToObj(i -> {
 					int x = MathUtils.random(Gdx.graphics.getWidth());
 					int y = MathUtils.random(Gdx.graphics.getHeight());
@@ -46,6 +49,7 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.begin();
 		me.render(batch);
 		enemies.forEach(enemy -> {
+
 			enemy.render(batch);
 			float xD = me.getPosition().x-enemy.getPosition().x;
 			float yD = me.getPosition().y-enemy.getPosition().y;
@@ -53,13 +57,35 @@ public class MyGdxGame extends ApplicationAdapter {
 			float normalX = xD / vector;
 			float normalY = yD / vector;
 			goTo.set(speedConst * normalX,speedConst * normalY);
-			enemy.moveTo(goTo);
+
+			if (collisionDetector(enemy, me)) {
+				enemy.moveTo(goTo);
+			}
 		});
+		if (constant.genNewEnemies) {
+			timeSeconds +=Gdx.graphics.getRawDeltaTime();
+			if(timeSeconds > period){
+				timeSeconds-=period;
+				int x = MathUtils.random(Gdx.graphics.getWidth());
+				int y = MathUtils.random(Gdx.graphics.getHeight());
+				mainChar newEnemy = new mainChar(x, y, "badlogic.jpg");
+				enemies.add(newEnemy);
+			}
+
+		}
 		batch.end();
 	}
 	@Override
 	public void dispose () {
 		batch.dispose();
 		me.dispose();
+	}
+	public boolean collisionDetector(mainChar obj1, mainChar obj2) {
+		if ((obj1.getPosition().x + size < obj2.getPosition().x || obj2.getPosition().x + size < obj1.getPosition().x)
+				||
+				(obj1.getPosition().y + size < obj2.getPosition().y || obj2.getPosition().y + size < obj1.getPosition().y)) {
+			return true;
+		}
+		return false;
 	}
 }
